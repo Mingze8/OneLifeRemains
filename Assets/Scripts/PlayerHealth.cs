48f03cc6d3c6572b0cebfecc09e81b9cc1de5e67
Eng Ming Ze <mingez18.2002@gmail.com>
2025-09-11 16:13:17 +0800

    Add Player Health Script

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int currentHealth;
+    public int maxHealth;
+    
+    public void changeHealth(int amount)
+    {
+        currentHealth += amount;
+        
+        if(currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
